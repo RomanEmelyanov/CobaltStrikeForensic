612471c473105c78c1c3f71f2fd6a3f0763c1c1c
RomanEmelyanov <RomanEmelyanov@users.noreply.github.com>
2018-04-15 16:46:21 +0300

    Add files via upload

--- DEMO.sh
+++ DEMO.sh
@@ -5,3 +5,8 @@ rm -rf output/*
 cat output/beacon1.txt
 ./sample2.sh
 cat output/beacon2.txt
+./sample3.sh
+cat output/procdump3.txt
+cat output/screenshot3.txt
+./sample4.sh
+cat output/beacon4_enc.txt
