24a17b10b315d39c5507c411ba6a35cf1aad2fca
RomanEmelyanov <RomanEmelyanov@users.noreply.github.com>
2018-04-16 09:54:38 +0300

    Add files via upload

--- sample4.sh
+++ sample4.sh
@@ -4,10 +4,9 @@
 ./L4/main.py --in_file=input/beacon4.html --out_file=output/beacon4_enc.dll
 objdump -p output/beacon4_enc.dll | grep ^Time > output/beacon4_enc.txt
 
-# TODO: fix align four BUG in L2_beacon_ECB.py , fast workaroud below
-#dd if=output/beacon4_enc.dll of=output/beacon4_enc2.dll skip=4109 bs=1
-dd if=output/beacon4_enc.dll of=output/beacon4_temp.dll skip=1 bs=4k
-tail -c +14 output/beacon4_temp.dll > output/beacon4_enc2.dll
+# TODO: fix align four BUG in L2_beacon_ECB.py , workaroud below
+#dd if=output/beacon4_enc.dll of=output/beacon4_enc2.dll skip=4109 bs=1 # too slow :(
+tail -c +4110 output/beacon4_enc.dll > output/beacon4_enc2.dll
 
 objdump -D -b binary -m intel -mi386 output/beacon4_enc2.dll | head -n 35 > output/beacon4_decoder.asm
 ./L2_beacon_CBC.py output/beacon4_enc2.dll output/beacon4.dll
